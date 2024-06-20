5114feda83bd0cd1720e5c0699fcad4dbb8eeab7
AryanDhawan <102812347+Aryancoder123@users.noreply.github.com>
2024-06-21 00:04:58 +0530

    done right!!

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
  
 const placeOrder = async(req,res)=>{
 
-    const frontend_url = "https://food-delivery-frontend-one.vercel.app"
+    const frontend_url = "https://food-delivery-frontend-17ve.onrender.com"
 
     try {
         const newOrder = new orderModel({
