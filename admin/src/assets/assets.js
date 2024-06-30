30583215bdb5bfdacaae90c09886df999d5caa7e
AryanDhawan <102812347+Aryancoder123@users.noreply.github.com>
2024-06-30 17:16:49 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-app-2-j5jx.onrender.com'
+export const url = 'https://food-delivery-backend-m2p7.onrender.com'
